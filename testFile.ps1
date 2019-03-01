f7fac7152058059206f37b93caff928a79aef621
John Mendez <jammer361@gmail.com>
2019-03-01 14:14:28 -0600

    iAdded Comments

--- testFile.ps1
+++ testFile.ps1
@@ -1,6 +1,6 @@
-$database = 'John'
-$server = '.'
-$table = 'dbo.Table_5'
+$database = '' #Specify Database ex. Test
+$server = '.'#Use . to specify the server you're on or specify unique server
+$table = '' #Specify Table Name ex. dbo.Table_4
 
 #Test to see if file was imported
-Invoke-Sqlcmd -Database $database -ServerInstance $server -Query "select * from $table"
+Invoke-Sqlcmd -Database $database -ServerInstance $server -Query "SELECT * from $table"
